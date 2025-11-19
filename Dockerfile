a4879bf4fcf4e863ec832043573fdeae0bbc8644
Чернышов Тимофей <tchernusho@gmail.com>
2025-11-20 01:28:03 +0300

    final fix

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,21 @@
-FROM python:3.11
+FROM python:3.11-slim
 
-ENV PYTHONUNBUFFERED=1
+ENV PYTHONUNBUFFERED=1 \
+    PIP_NO_CACHE_DIR=1
 
 WORKDIR /app
-COPY req.txt /app/
-RUN pip install --upgrade pip && pip install -r req.txt
 
-COPY app/ /app/
+# Устанавливаем зависимости Linux (часто нужны для aiohttp)
+RUN apt-get update && apt-get install -y --no-install-recommends \
+    build-essential \
+    && rm -rf /var/lib/apt/lists/*
 
-# Запуск main-файла
+# Копируем зависимости
+COPY req.txt .
+
+RUN pip install -r req.txt
+
+# Копируем весь код
+COPY app/ .
+
 CMD ["python", "bot_api.py"]
--- app/bot_api.py
+++ app/bot_api.py
@@ -1,23 +1,51 @@
 import asyncio
 import time
 import os
+import requests
 
 from aiogram import Bot
-from parser_html import get_mcap_number
 from dotenv import load_dotenv
 
 load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 CHAT_ID = os.getenv("CHAT_ID")
-INTERVAL = 15
+INTERVAL = 180
 
 
-async def send_mcap(bot: Bot, value: float):
-    if value > 800:
-        text = f"<b>MCAP:</b> {value:,.2f} тыс. $"
+def get_mcap_number() -> int | None:
+    url = "https://api.dexscreener.com/tokens/v1/ton/EQDxE8C4-R53ol4InsjyVedmfI9xMF6lpJko2p90PAR0U5NT"
+
+    try:
+        response = requests.get(url, headers={"Accept": "*/*"}, timeout=10)
+        response.raise_for_status()
+
+        data = response.json()
+        if not data:
+            print("[!] Пары не найдены")
+            return None
+
+        market_cap = data[0]["marketCap"]
+        return int(market_cap)
+
+    except requests.exceptions.RequestException as e:
+        print(f"[!] Сетевая ошибка: {e}")
+    except (KeyError, IndexError, TypeError, ValueError):
+        print("[!] Ошибка парсинга ответа API")
+
+    return None
+
+
+async def send_mcap(bot: Bot, value: int, last_mcap):
+    if value > last_mcap:
+        text = '🟢📈'
+    elif value < last_mcap:
+        text = '🔴📉'
+
+    if value % 1_000_000 >= 1:
+        text += f"<b>MCAP:</b> {value // 1_000:,} млн. $"
     else:
-        text = f"<b>MCAP:</b> {value:,.2f} млн. $"
+        text += f"<b>MCAP:</b> {value // 1_000:,} тыс. $"
 
     await bot.send_message(chat_id=CHAT_ID, text=text, parse_mode="HTML")
 
@@ -27,19 +55,23 @@ async def monitor():
     last_mcap = None
 
     print("Бот запущен. Мониторинг MCAP...")
+
     try:
         while True:
             mcap = get_mcap_number()
             if mcap is None:
                 print(f"[{time.strftime('%H:%M:%S')}] Нет данных")
+
             elif mcap != last_mcap:
-                await send_mcap(bot, mcap)
-                print(f"[{time.strftime('%H:%M:%S')}] Отправлено: {mcap:,.2f} тыс. $")
+                await send_mcap(bot, mcap, last_mcap)
+                print(f"[{time.strftime('%H:%M:%S')}] Отправлено: {mcap:,.2f} $")
                 last_mcap = mcap
+
             else:
-                print(f"[{time.strftime('%H:%M:%S')}] Без изменений: {mcap:,.2f} тыс. $")
+                print(f"[{time.strftime('%H:%M:%S')}] Без изменений: {mcap:,.2f} $")
 
             await asyncio.sleep(INTERVAL)
+
     except KeyboardInterrupt:
         print("\nОстановлено вручную.")
     finally:
